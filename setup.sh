b13e8da3ab054e9b8d4892e34f8a30e0f37bf1af
Richard Hyde <email@richardhyde.net>
2021-11-30 14:18:36 +0000

    Added mklink function to create/copy files

--- setup.sh
+++ setup.sh
@@ -2,5 +2,30 @@
 
 CURR_DIR=$(pwd)
 
-ln -s ${CURR_DIR}fish ${HOME}/.config/fish
+mklink() {
+	SRC=$1
+	
+	if [ -z "$2" ]; then
+		DEST=$1
+	else
+		DEST=$2
+	fi
+
+	if [ -h ${HOME}/${DEST} -o ${HOME}/${DEST} -ef ${CURR_DIR}/${SRC} ]; then
+		return 0
+	fi
+
+	if [ -d ${CURR_DIR}/${SRC} ]; then
+		ln -s ${CURR_DIR}/${SRC} ${HOME}/${DEST}
+	else
+		ln ${CURR_DIR}/${SRC} ${HOME}/${DEST}
+	fi
+}
+
+if [ ! -d ${HOME}/.config ]; then
+	mkdir ~/.config
+fi
+
+mklink fish .config/fish
+
 
