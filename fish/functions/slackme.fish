a1fcbeda2110813476475799a9420f8448ddc7bf
Richard Hyde <email@richardhyde.net>
2022-01-01 15:01:23 +0000

    Deleted slackme.fish

--- fish/functions/slackme.fish
+++
@@ -1,5 +0,0 @@
-function slackme
-	set payload "{ \\\"username\\\": \\\"$hostname\\\", \\\"text\\\": \\\"$argv[1]\\\" }"
-	set cmd "curl -X POST https://hooks.slack.com/services/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 -d \"$payload\" -H \"Content-Type: application/json\""
-	eval $cmd
-end
